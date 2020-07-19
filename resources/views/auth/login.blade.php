77fc557fd2c7ed948b9c4b05a7d0bbcbaf93d0e5
joselfonseca <jose.fonseca@domandtom.com>
2020-07-19 15:10:03 -0500

    delete header

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,18 +2,6 @@
 
 @section('content')
     <div class="container mx-auto my-6">
-        <div class="sm:mx-auto sm:w-full sm:max-w-md">
-            <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-on-white.svg" alt="Workflow">
-            <h2 class="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900">
-                {{ __("Sign in to your account")}}
-            </h2>
-            <p class="mt-2 text-center text-sm leading-5 text-gray-600 max-w">
-                {{ __("Or")}}
-                <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">
-                {{ __("start your 14-day free trial")}}
-                </a>
-            </p>
-        </div>
         <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
             <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
               <form method="POST" action="{{ route('login') }}">

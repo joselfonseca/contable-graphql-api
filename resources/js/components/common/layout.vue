56f3075bef5bce6c095187439fb027a752d3c77b
joselfonseca <jose.fonseca@domandtom.com>
2020-07-18 19:44:15 -0500

    WIP

--- resources/js/components/common/layout.vue
+++ resources/js/components/common/layout.vue
@@ -5,7 +5,7 @@
         <slot name="header"></slot>
       </div>
     </header>
-    <main class="h-full">
+    <main class="container mx-auto h-full">
       <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
         <slot name="content"></slot>
       </div>

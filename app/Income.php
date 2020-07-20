cc53ff962a569c3e93648f18dff89a2a37c3ffde
joselfonseca <jose.fonseca@domandtom.com>
2020-07-20 17:20:30 -0500

    add Income model and migration with some cleanup

---
+++ app/Income.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Income extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'category_id',
+        'account_id',
+        'date',
+        'amount',
+        'description'
+    ];
+
+    public function user() : BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function account() : BelongsTo
+    {
+        return $this->belongsTo(Account::class);
+    }
+
+    public function category() : BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
